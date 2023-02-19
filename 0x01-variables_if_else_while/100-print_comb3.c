4d45bbf2a1dbb6de40023e60a269854c375ba1d1
Ian-Macharia <inmacharia@gmail.com>
2023-02-19 04:34:10 -0800

    all combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (x != y)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+}
