60ba76f713a679030bb608d6579317bc521c8a1c
Ian-Macharia <inmacharia@gmail.com>
2023-02-19 04:05:37 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar('0' + x);
+	}
+	for (x = 'A'; x <= 'F'; x++)
+	{
+		putchar(tolower(x));
+	}
+
+	putchar('\n');
+	return (0);
+}
