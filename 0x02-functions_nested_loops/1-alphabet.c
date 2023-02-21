168c7792434f1ea57fdc949c38f9d51b59623eff
Ian-Macharia <inmacharia@gmail.com>
2023-02-21 11:45:51 -0800

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		_putchar(tolower(x));
+	}
+	_putchar('\n');
+	return (0);
+}
