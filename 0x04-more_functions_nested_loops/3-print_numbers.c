020b45044cdd95d54ad81eacdcf3ed56f5a87600
Ian-Macharia <inmacharia@gmail.com>
2023-02-23 17:08:32 -0800

    Multiply numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints numbers
+  *
+  * Return: Result of multiplication
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
