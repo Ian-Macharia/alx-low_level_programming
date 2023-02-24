bddbf4922fba2498012549a57a79580c9e6c0a3d
Ian-Macharia <inmacharia@gmail.com>
2023-02-23 18:10:35 -0800

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_line - prints line
+  * @n: no. of times line to be printed
+  * Return: Printed line
+  */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+
+}
